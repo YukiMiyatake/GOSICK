a2cd2ee000de95424e5130c8ad750719e9383574
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2019-08-22 03:33:34 +0900

    refactor

--- src/util/cmd/cmd.go
+++ src/util/cmd/cmd.go
@@ -1,30 +1,33 @@
 package main
 
+// Refactor
 import (
-	"log"
 	"bufio"
+	"log"
 	"os"
 	"strings"
-	 "github.com/YukiMiyatake/GOSICK/util"
+
+	"util"
+	slackUtil "util/slack"
 )
 
 func init() {
 	log.Printf("[Info] Start CommandLine driver ")
 }
 
-func Echo(sc util.SlackConfig, pm *util.PluginManager){
+func Echo(sc slackUtil.SlackConfig, pm *util.PluginManager) {
 	stdin := bufio.NewScanner(os.Stdin)
-	for stdin.Scan(){
+	for stdin.Scan() {
 		text := stdin.Text()
 		log.Print(text)
-		if(strings.ToLower(text) == "quit"){
+		if strings.ToLower(text) == "quit" {
 			break
 		}
 
-		msgs := strings.Fields( text )
+		msgs := strings.Fields(text)
 		// TODO: load from Env or JSON
-		md := util.NewMessageDispatcher( &sc.BotName, &sc.BotID)
-		if (md.GetMessageType(msgs[0]) == util.Mention) {
+		md := util.NewMessageDispatcher(&sc.BotName, &sc.BotID)
+		if md.GetMessageType(msgs[0]) == util.Mention {
 			for key, value := range pm.Mention {
 				if msgs[1] == key {
 					log.Print(value.(func([]string) string)(msgs[2:]))
